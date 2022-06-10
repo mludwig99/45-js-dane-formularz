1813786a68fc30db17eec13d5eb082b67ade2cf9
mludwig99 <michalludwig99@gmail.com>
2022-06-10 22:30:55 +0200

    45-js-dane-formularz

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let pName = document.getElementById('name');
+let pSurname = document.getElementById('surname');
+let pPhone = document.getElementById('phone');
+const button = document.querySelector('button');
+
+ function dataBase() {
+     let imie = document.getElementById('fname');
+     let nazwisko = document.getElementById('lname');
+     let tel = document.getElementById('phoneNumber');
+     pName.textContent = imie.value;
+     pSurname.textContent = nazwisko.value;
+     pPhone.textContent = tel.value;
+
+   
+
+}
+
+button.addEventListener('click', dataBase);
